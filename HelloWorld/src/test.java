f3c5a57904c9dd100396139cc4c416f6238e9468
AlexjandroN7 <alejandro.urdiales.93@gmail.com>
2017-02-17 20:10:35 +0100

    Third

--- HelloWorld/src/test.java
+++ HelloWorld/src/test.java
@@ -4,11 +4,13 @@ public class test {
 	
 	private int test1;
 	private String testS;
+	private int testC;
 	
 	public test(int a, String b) {
 		
 		this.test1 = a;
 		this.testS = b;
+		this.testC = c;
 		
 	}
 }
